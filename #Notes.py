f2cb74902735faf2f45747e2d261c8f8e61a24f3
Davan Hennessy Hwang <87882600+Schmut@users.noreply.github.com>
2024-06-06 16:19:14 -0400

    Update #Notes.py

--- #Notes.py
+++ #Notes.py
@@ -1,11 +1,27 @@
+import requests
+
 #Notes
 #Note Maker program for premade notes 
+
+class Pax8():
+    def __init__(self,requester,customer):
+        self.requester = requester
+        self.customer = customer
+
+    def Pax8_GDAP(self):
+        print("Connected to Pax 8")
+        print("Created tenant account in Pax 8")
+        print("Connected to Office 365 admin center")
+        print("Created GDAP relationship with Pax 8 and Office 365")
+        print("")
+
 class Email():
     #initializes variables
-    def __init__(self,User,requester,distro_email=None):
+    def __init__(self,User,requester,distro_email=None,Pax8_license=None):
         self.User = User
         self.requester = requester
         self.distro_email = distro_email
+        self.Pax8_license = Pax8_license
     #Create email account notes
     def create(self):
         print(requester,"requested for",User,"to be created")
@@ -36,6 +52,12 @@ class Email():
         print(requester,"requested for",User,"to be added to",distro_email)
         print("Connected to Office 365 admin center")
         print("Added",User,"to",distro_email,"distribution list")
+        print("")
+    #Pax 8 license
+    def pax8(self):
+        print(requester,"requested for",User,"to have",Pax8_license,"added to their account")
+        print("Connected to Pax 8")
+        print("Added",Pax8_license,"to",User +"'s account")
         print("")
 
 class AD():
@@ -72,6 +94,7 @@ while True:
     print ("Note Maker")
     User = input("Who is the user: ")
     requester = input("Who sent in the request: ")
+    customer = "Customer"
     print("")
     options = input("""Choose an option
 1. Email Creation
@@ -80,12 +103,19 @@ while True:
 4. Active Directory termination
 5. Reset email password
 6. Reset active directory password
-7. Add User to distribution list 
+7. Add User to distribution list
+8. Pax 8 License
+9. Pax 8 tenant creation
 Option: """)
 
     if options == "7":
         print("")
         distro_email = input("What is the distribution list email?: ")
+        print("")
+
+    if options == "8":
+        print("")
+        Pax8_license = input("What license did you purchase?: ")
         print("")
 
     #connects option with text
@@ -117,6 +147,14 @@ Option: """)
         print("")
         e=Email(User,requester,distro_email)
         e.distro()
+    elif options =="8":
+        print("")
+        e=Email(User,requester,Pax8_license)
+        e.pax8()
+    elif options =="9":
+        print("")
+        p=Pax8(requester,customer)
+        p.Pax8_GDAP()
     else:
         print("")
         print("Option not available")
@@ -126,3 +164,5 @@ Option: """)
         break
     elif again == "y":
         pass
+    else:
+        print("incorrect input")
